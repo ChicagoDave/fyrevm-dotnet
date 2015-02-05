e0de6f66aca672937440e08e05fb7a6b7d286890
David Cornelson <david.cornelson@gmail.com>
2015-02-05 15:05:24 -0600

    Added FyreChannels and new test ulx file.
    Run any current ULX file with FyreChannels to test all channel data.

---
+++ FyreChannels/MainForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace SimpleFyre
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutSimpleFyreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.inputPanel = new System.Windows.Forms.Panel();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ChannelName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChannelData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.inputPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1043, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openGameToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openGameToolStripMenuItem
+            // 
+            this.openGameToolStripMenuItem.Name = "openGameToolStripMenuItem";
+            this.openGameToolStripMenuItem.Size = new System.Drawing.Size(166, 24);
+            this.openGameToolStripMenuItem.Text = "&Open Game...";
+            this.openGameToolStripMenuItem.Click += new System.EventHandler(this.openGameToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(163, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(166, 24);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutSimpleFyreToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutSimpleFyreToolStripMenuItem
+            // 
+            this.aboutSimpleFyreToolStripMenuItem.Name = "aboutSimpleFyreToolStripMenuItem";
+            this.aboutSimpleFyreToolStripMenuItem.Size = new System.Drawing.Size(205, 24);
+            this.aboutSimpleFyreToolStripMenuItem.Text = "&About SimpleFyre...";
+            this.aboutSimpleFyreToolStripMenuItem.Click += new System.EventHandler(this.aboutSimpleFyreToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.txtTime, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtScore, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.inputPanel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtLocation, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(162, 24);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(155, 3, 3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(881, 500);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTime.Enabled = false;
+            this.txtTime.Location = new System.Drawing.Point(774, 3);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.ReadOnly = true;
+            this.txtTime.Size = new System.Drawing.Size(48, 20);
+            this.txtTime.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.txtTime, "Time");
+            // 
+            // txtScore
+            // 
+            this.txtScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtScore.Enabled = false;
+            this.txtScore.Location = new System.Drawing.Point(830, 3);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.ReadOnly = true;
+            this.txtScore.Size = new System.Drawing.Size(48, 20);
+            this.txtScore.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.txtScore, "Score");
+            // 
+            // inputPanel
+            // 
+            this.inputPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.inputPanel, 3);
+            this.inputPanel.Controls.Add(this.lblPrompt);
+            this.inputPanel.Controls.Add(this.txtInput);
+            this.inputPanel.Location = new System.Drawing.Point(3, 474);
+            this.inputPanel.Name = "inputPanel";
+            this.inputPanel.Size = new System.Drawing.Size(875, 23);
+            this.inputPanel.TabIndex = 5;
+            this.inputPanel.Resize += new System.EventHandler(this.ArrangeInput);
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(3, 4);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(46, 15);
+            this.lblPrompt.TabIndex = 2;
+            this.lblPrompt.Text = "prompt";
+            this.lblPrompt.Resize += new System.EventHandler(this.ArrangeInput);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Enabled = false;
+            this.txtInput.Location = new System.Drawing.Point(80, 1);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(527, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLocation.Enabled = false;
+            this.txtLocation.Location = new System.Drawing.Point(3, 3);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.ReadOnly = true;
+            this.txtLocation.Size = new System.Drawing.Size(764, 20);
+            this.txtLocation.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txtLocation, "Location");
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ChannelName,
+            this.ChannelData});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(764, 440);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ChannelName
+            // 
+            this.ChannelName.HeaderText = "Channel";
+            this.ChannelName.Name = "ChannelName";
+            this.ChannelName.ReadOnly = true;
+            // 
+            // ChannelData
+            // 
+            this.ChannelData.HeaderText = "Channel Data";
+            this.ChannelData.Name = "ChannelData";
+            this.ChannelData.ReadOnly = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 524);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Fyre Channels";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.inputPanel.ResumeLayout(false);
+            this.inputPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutSimpleFyreToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel inputPanel;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChannelName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChannelData;
+    }
+}
+
---
+++ FyreChannels/MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using FyreVM;
+
+namespace SimpleFyre
+{
+    public partial class MainForm : Form
+    {
+        private Engine vm;
+        private Thread vmThread;
+        private Stream gameFile;
+
+        private AutoResetEvent inputReadyEvent = new AutoResetEvent(false);
+        private bool lineWanted = false;
+        private bool keyWanted = false;
+        private string inputLine = null;
+        private string[] channelText = new string[18];
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            lblPrompt.Text = "";
+            txtInput.Bounds = inputPanel.ClientRectangle;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutSimpleFyreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("SimpleFyre\nA FyreVM sample application.");
+        }
+
+        private void openGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filename;
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Select Game File";
+                dlg.Filter = "Glulx files (*.ulx)|*.ulx";
+
+                if (dlg.ShowDialog() == DialogResult.Cancel)
+                    return;
+
+                filename = dlg.FileName;
+            }
+
+            if (vmThread != null)
+            {
+                vmThread.Abort();
+                vmThread.Join();
+            }
+
+            if (gameFile != null)
+                gameFile.Close();
+
+            txtInput.Clear();
+            txtLocation.Clear();
+            txtTime.Clear();
+            txtScore.Clear();
+
+            gameFile = new FileStream(filename, FileMode.Open, FileAccess.Read);
+
+            vm = new Engine(gameFile);
+            //vm.OutputFilterEnabled = false;
+
+            vm.LineWanted += new LineWantedEventHandler(vm_LineWanted);
+            vm.KeyWanted += new KeyWantedEventHandler(vm_KeyWanted);
+            vm.OutputReady += new OutputReadyEventHandler(vm_OutputReady);
+            vm.SaveRequested += new SaveRestoreEventHandler(vm_SaveRequested);
+            vm.LoadRequested += new SaveRestoreEventHandler(vm_LoadRequested);
+
+            vmThread = new Thread(VMThreadProc);
+            vmThread.IsBackground = true;
+            vmThread.Start();
+        }
+
+        private void RequestLine()
+        {
+            lineWanted = true;
+            keyWanted = false;
+
+            txtInput.Enabled = true;
+            txtInput.Focus();
+        }
+
+        private void RequestKey()
+        {
+            lineWanted = false;
+            keyWanted = true;
+
+            txtInput.Enabled = true;
+            txtInput.Focus();
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (keyWanted)
+            {
+                GotInput(new string(e.KeyChar, 1));
+                e.Handled = true;
+            }
+            else if (e.KeyChar == '\r' || e.KeyChar == '\n')
+            {
+                // already handled in KeyDown
+                e.Handled = true;
+            }
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                e.Handled = true;
+
+                if (keyWanted)
+                {
+                    GotInput("\n");
+                }
+                else if (lineWanted)
+                {
+                    GotInput(txtInput.Text);
+                    //txtOutput.Text += lblPrompt.Text + txtInput.Text + "\r\n";
+                    txtInput.Clear();
+                }
+            }
+        }
+
+        private void GotInput(string line)
+        {
+            txtInput.Enabled = false;
+            inputLine = line;
+            inputReadyEvent.Set();
+        }
+
+        public class Channel
+        {
+            public Channel(string name, string data)
+            {
+                this.ChannelName = name;
+                this.ChannelData = data;
+            }
+
+            public string ChannelName { get; set; }
+            public string ChannelData { get; set; }
+        }
+
+        private void HandleOutput(Dictionary<string, string> package)
+        {
+            List<Channel> channels = new List<Channel>();
+
+            foreach (string key in package.Keys)
+            {
+                channels.Add(new Channel(key, package[key]));
+            }
+
+            dataGridView1.DataSource = channels;
+        }
+
+        private void ArrangeInput(object sender, EventArgs e)
+        {
+            txtInput.Left = lblPrompt.Right + 2;
+            txtInput.Width = inputPanel.ClientSize.Width - txtInput.Left;
+        }
+
+        private Stream RequestSaveStream()
+        {
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.Title = "Select Save File";
+                dlg.Filter = "Quetzal save files (*.sav)|*.sav";
+
+                if (dlg.ShowDialog() == DialogResult.Cancel)
+                    return null;
+                else
+                    return new FileStream(dlg.FileName, FileMode.Create, FileAccess.Write);
+            }
+        }
+
+        private Stream RequestLoadStream()
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Load a Saved Game";
+                dlg.Filter = "Quetzal save files (*.sav)|*.sav";
+
+                if (dlg.ShowDialog() == DialogResult.Cancel)
+                    return null;
+                else
+                    return new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read);
+            }
+        }
+
+        #region VM Thread
+
+        private void VMThreadProc(object dummy)
+        {
+            try
+            {
+                vm.Run();
+                this.Invoke(new Action(GameFinished));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void vm_LineWanted(object sender, LineWantedEventArgs e)
+        {
+            this.Invoke(new Action(RequestLine));
+            inputReadyEvent.WaitOne();
+            e.Line = inputLine;
+        }
+
+        private void vm_KeyWanted(object sender, KeyWantedEventArgs e)
+        {
+            this.Invoke(new Action(RequestKey));
+            inputReadyEvent.WaitOne();
+            e.Char = inputLine[0];
+        }
+
+        private void vm_OutputReady(object sender, OutputReadyEventArgs e)
+        {
+            this.Invoke(
+                new Action<Dictionary<string, string>>(HandleOutput),
+                e.Package);
+        }
+
+        private void vm_SaveRequested(object sender, SaveRestoreEventArgs e)
+        {
+            e.Stream = (Stream)this.Invoke(new Func<Stream>(RequestSaveStream));
+        }
+
+        private void vm_LoadRequested(object sender, SaveRestoreEventArgs e)
+        {
+            e.Stream = (Stream)this.Invoke(new Func<Stream>(RequestLoadStream));
+        }
+
+        #endregion
+
+        private void GameFinished()
+        {
+            this.Close();
+        }
+
+    }
+}
